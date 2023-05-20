874b488cc433714a482787c24887685adb4d265c
Star <devarise1@gmail.com>
2023-05-20 14:15:08 +0100

    added resources arrray

---
+++ src/data.ts
@@ -0,0 +1,60 @@
+import img4 from "./assets/img4.jpg";
+import img5 from "./assets/img5.jpg";
+import img6 from "./assets/img6.jpg";
+import img7 from "./assets/img7.jpg";
+import img8 from "./assets/img8.jpg";
+import img9 from "./assets/img9.jpg";
+
+const watchers: Watcher[] = [
+  {
+    id: "1",
+    name: "Oladotun",
+    image:
+      "https://user-images.githubusercontent.com/24845008/188237668-8f34c2a7-f79c-4e0f-9e18-fe8768142d11.png",
+  },
+  {
+    id: "2",
+    name: "Avos",
+    image:
+      "https://user-images.githubusercontent.com/24845008/188237859-c4d6aff1-c1d8-49df-9bef-67fa1e44e6f4.png",
+  },
+  {
+    id: "3",
+    name: "Codexive",
+    image:
+      "https://user-images.githubusercontent.com/24845008/188238192-bfe86e93-4fc9-43e9-b4b3-740e3de97ceb.png",
+  },
+
+  {
+    id: "4",
+    name: "Mr Learned",
+    image: img4,
+  },
+  {
+    id: "5",
+    name: "Mayowa",
+    image: img5,
+  },
+  {
+    id: "6",
+    name: "Victor",
+    image: img6,
+  },
+  {
+    id: "7",
+    name: "Mec",
+    image: img7,
+  },
+  {
+    id: "8",
+    name: "Daniel",
+    image: img8,
+  },
+  {
+    id: "9",
+    name: "Moses",
+    image: img9,
+  },
+];
+
+export default watchers;
